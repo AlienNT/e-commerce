062e0f5676f713a533bc37e299c4bbfc5021a6ec
Nick Tristen <mtrysten.dev@gmail.com>
2024-07-22 21:57:03 +0300

    add (components/UI): add search field component

---
+++ src/components/UI/SearchField.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+interface SearchFieldProps {
+  placeholder: string,
+  value?: string
+}
+
+withDefaults(defineProps<SearchFieldProps>(), {
+  placeholder: 'What are you looking for?'
+})
+</script>
+
+<template>
+  <div class="search-field">
+    <label class="search-label">
+      <input
+          class="search-input"
+          type="search"
+          :placeholder="placeholder"
+      >
+    </label>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.search-label {
+  padding: 7px 12px 7px 20px;
+  background: $colorSecondaryLight;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.search-input {
+  &::placeholder {
+    transition: .2s ease;
+  }
+
+  &:focus {
+    &::placeholder {
+      color: transparent;
+    }
+  }
+}
+
+</style>
