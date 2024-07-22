b3d8015b312e2a9d6d8bfe464d2554e217aaf6a8
Nick Tristen <mtrysten.dev@gmail.com>
2024-07-22 21:53:48 +0300

    add (pages): add about, contact, home, sign up & not found page components

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="not-found-page">
+  <div class="not-found-page__title">404 Not Found</div>
+  <div class="not-found-page__content">Your visited page not found. You may go home page.</div>
+  <button
+      type="button"
+      class="not-found-page__button"
+      @click.stop="$router.push('/')"
+  >
+    Back to home page
+  </button>
+</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
