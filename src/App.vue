8c0ae9bf47c5dd07f12d504812d343465b2a0907
Nick Tristen <mtrysten.dev@gmail.com>
2024-07-22 21:55:17 +0300

    update (app): add main components to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+
+import TheMain from "@/components/TheMain.vue";
+import TheTopHeader from "@/components/TheTopHeader.vue";
+import TheHeader from "@/components/TheHeader.vue";
+import TheFooter from "@/components/TheFooter.vue";
 </script>
 
 <template>
-  App
+  <TheTopHeader/>
+  <TheHeader/>
+  <TheMain/>
+  <TheFooter/>
 </template>
 
-<style scoped>
+<style lang="scss">
+#app {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  background: $colorBackground;
+}
+.footer {
+  margin-top: auto;
+}
 </style>
