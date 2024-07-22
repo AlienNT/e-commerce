c5960fbf97d2dc69798e6c560b67734fffab9176
Nick Tristen <mtrysten.dev@gmail.com>
2024-07-22 21:55:38 +0300

    add (components): add navigation components

---
+++ src/components/Navigation/NavigationList.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+import NavigationListItem from "@/components/Navigation/NavigationListItem.vue";
+import {NavigationListItemInterface} from "@/types/navigation/types.ts";
+
+interface NavigationListProps {
+  navigationList: NavigationListItemInterface[]
+}
+
+defineProps<NavigationListProps>()
+</script>
+
+<template>
+  <nav class="navigation">
+    <ul class="navigation-list">
+      <NavigationListItem
+          v-for="({path, title, name}, index) in navigationList"
+          :title="title"
+          :path="path"
+          :name="name"
+          :key="index"
+      />
+    </ul>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+.navigation {
+  display: flex;
+  flex: 1;
+}
+.navigation-list {
+  display: flex;
+  gap: 15px;
+  flex: 1;
+  justify-content: space-around;
+}
+</style>
---
+++ src/components/Navigation/NavigationListItem.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {NavigationListItemInterface} from "@/types/navigation/types.ts";
+
+interface NavigationListItemProps extends NavigationListItemInterface {
+  activeClass?: string;
+}
+
+withDefaults(defineProps<NavigationListItemProps>(), {
+  activeClass: 'link-active',
+})
+
+</script>
+
+<template>
+  <li class="navigation-list-item">
+    <RouterLink
+        :to="path"
+        :active-class="activeClass"
+        class="navigation-link"
+    >
+      {{ title || name }}
+    </RouterLink>
+  </li>
+</template>
+
+<style scoped lang="scss">
+.navigation-link {
+  /* Title/16PX Regular */
+  color: rgb(0, 0, 0);
+  font-family: Poppins, sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+  text-align: center;
+  display: flex;
+  transition: .2s ease;
+  border-bottom: 2px solid transparent;
+}
+.link-active, .navigation-link:hover {
+  border-bottom-color: rgba(0, 0, 0, 0.6);
+}
+</style>
