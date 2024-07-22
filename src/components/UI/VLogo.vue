9c57f8b355e49a11469257c175a331c0ea05fb1c
Nick Tristen <mtrysten.dev@gmail.com>
2024-07-22 21:57:11 +0300

    add (components/UI): add text logo component

---
+++ src/components/UI/VLogo.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface LogoPropsInterface {
+  text: string
+}
+
+defineProps<LogoPropsInterface>()
+</script>
+
+<template>
+  <div class="text-logo">
+    {{ text }}
+  </div>
+</template>
+
+<style scoped lang="scss">
+.text-logo {
+  /* Heading/24PX Bold */
+  color: rgb(0, 0, 0);
+  font-family: Inter,sans-serif;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 24px;
+  letter-spacing: 1px;
+  text-align: left;
+}
+</style>
