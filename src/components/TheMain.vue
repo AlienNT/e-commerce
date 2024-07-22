dd4aa988ddcfd7d133454ca6985a155e006f86ff
Nick Tristen <mtrysten.dev@gmail.com>
2024-07-22 21:57:34 +0300

    add (components): add main component

---
+++ src/components/TheMain.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<main class="main">
+  <div class="container">
+    <RouterView/>
+  </div>
+</main>
+</template>
+
+<style scoped>
+.main {
+  min-height: 150vh;
+}
+</style>
