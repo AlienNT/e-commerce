0ebb7e626a8db60deb1a0fc8dceb0a69b67f962d
Nick Tristen <mtrysten.dev@gmail.com>
2024-07-22 21:57:41 +0300

    add (components): add top header component

---
+++ src/components/TheTopHeader.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="top-header">
+  <div class="container">
+    Top Header
+  </div>
+</div>
+</template>
+
+<style scoped lang="scss">
+.top-header {
+  padding-top: 15px;
+  padding-bottom: 15px;
+  background: $colorTextDark;
+  color: $colorTextLight;
+  margin-bottom: 40px;
+}
+</style>
