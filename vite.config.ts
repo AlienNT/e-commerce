7e7518c4093fe6b3e55760b80ce3f85acd2cbec3
Nick Tristen <mtrysten.dev@gmail.com>
2024-07-22 21:58:19 +0300

    add (tsconfig): add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'node:url'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+import autoprefixer from "autoprefixer";
+
+export default defineConfig(({mode, command}) => {
+  console.log('vite config: ', {mode, command})
+  return {
+    plugins: [vue()],
+    css: {
+      postcss: {
+        plugins: [autoprefixer()]
+      },
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/css/variables/variables.scss";`
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
+  }
 })
